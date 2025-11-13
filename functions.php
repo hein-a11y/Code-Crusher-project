9b6145c7a19bad07a2353d851578970c9b794663
Kaung Si Thu Lin <2505049@i-seifu.jp>
2025-11-13 13:21:24 +0900

    latest update of sign up feature

--- functions.php
+++ functions.php
@@ -10,4 +10,48 @@ function getPDO() {
     return new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",
                     $id, $password);
 }
+
+function debug($data){
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+}
+
+function h($string){
+    return htmlspecialchars($string);
+}
+
+function isActiveMember(PDO $pdo, int $user_id): bool
+{
+    $sql = $pdo->prepare("SELECT 1 FROM gg_premium WHERE user_id = :user_id AND is_active = TRUE LIMIT 1");
+    
+    // Bind the user ID parameter
+    $sql->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    
+    //  Execute the query
+    $sql->execute();
+    
+    // Check the result
+    // fetchColumn() returns the value of the first column ('1' in this case) if a row is found, 
+    // or FALSE if no rows are found. We cast the result to a boolean.
+    return (bool) $sql->fetchColumn();
+}
+
+function hasBought(PDO $pdo, int $user_id, int $product_id,string $type): bool
+{
+    $sql = $pdo->prepare("SELECT orders.user_id,detail.game_id,detail.gadget_id FROM gg_detail_orders as detail
+                             INNER JOIN gg_orders AS orders ON orders.order_id = detail.order_id
+                              WHERE orders.user_id = :user_id AND detail.".$type." = :product_id;");
+    
+    // Bind the user ID parameter
+    $sql->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $sql->bindParam(':product_id', $product_id, PDO::PARAM_INT);
+    //  Execute the query
+    $sql->execute();
+    
+    // Check the result
+    // fetchColumn() returns the value of the first column ('1' in this case) if a row is found, 
+    // or FALSE if no rows are found. We cast the result to a boolean.
+    return (bool) $sql->fetchColumn();
+}
 ?>
